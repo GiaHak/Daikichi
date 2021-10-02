607e6bd82f80da07316d0f6be76ccd0d6df2bc3c
GiaHak <giahak@yahoo.com>
2021-10-01 20:57:54 -0400

    initial

--- src/main/java/com/gia/daikichi/controllers/DaikichiController.java
+++ src/main/java/com/gia/daikichi/controllers/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.gia.daikichi.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,14 @@ public class DaikichiController {
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to open to new ideas!";
 	}
-
+	 @RequestMapping("/travel/{location}")
+	 public String travel(@PathVariable("location") String location) {
+		 return "Congratulations! You will soon travel to " + location + ".";
+	 }
+	 @RequestMapping("/lotto/{number}")
+	 public String number(@PathVariable("number") int number) {
+		 if ( number%2 == 0)
+			 return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		 return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+	 }
 }
